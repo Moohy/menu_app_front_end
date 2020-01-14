09b31f8adc1e4b29ae71b0c4dcaac1aa86f87804
Mohammed <moohyacc@gmail.com>
2020-01-14 19:31:24 +0300

    Updates

---
+++ precache-manifest.7822b674cb4dedd1db0fe19d8d979986.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "366c0d314b7705cbfc3b0f9dfefaef1d",
+    "url": "/menu_app_front_end/index.html"
+  },
+  {
+    "revision": "7405e7d78e884ed07e70",
+    "url": "/menu_app_front_end/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "dd00cd996aad271da546",
+    "url": "/menu_app_front_end/static/css/main.26bc767b.chunk.css"
+  },
+  {
+    "revision": "7405e7d78e884ed07e70",
+    "url": "/menu_app_front_end/static/js/2.3995fafc.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/menu_app_front_end/static/js/2.3995fafc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd00cd996aad271da546",
+    "url": "/menu_app_front_end/static/js/main.b3b284b1.chunk.js"
+  },
+  {
+    "revision": "7f9b46426df24de6cf12",
+    "url": "/menu_app_front_end/static/js/runtime-main.05f5429d.js"
+  },
+  {
+    "revision": "babb8abad4f01d825bfbe6a9c2f33dd5",
+    "url": "/menu_app_front_end/static/media/image2.babb8aba.jpg"
+  },
+  {
+    "revision": "8d832dd4557266b637de28bf56e3e8e1",
+    "url": "/menu_app_front_end/static/media/image3.8d832dd4.png"
+  },
+  {
+    "revision": "151f3e580e13f34a9fcae8f91e2fdeed",
+    "url": "/menu_app_front_end/static/media/image4.151f3e58.jpg"
+  }
+]);
